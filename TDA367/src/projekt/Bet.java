5f320ab9084d51c0b15f141c0fb9fa82960b8235
Johan Gustavsson <johang87@hotmail.com>
2012-03-15 12:01:19 +0100

    Nytt projekt

---
+++ TDA367/src/projekt/Bet.java
@@ -0,0 +1,14 @@
+package projekt;
+
+public class Bet {
+	
+	private int steps;
+	
+	public Bet(int steps){
+		this.steps = steps;
+	}
+	
+	public int getBetValue(){
+		return steps;
+	}
+}
---
+++ TDA367/src/projekt/Mission.java
@@ -0,0 +1,11 @@
+package projekt;
+
+public class Mission {
+	
+	public Mission(){
+		
+	}
+	
+	
+
+}
