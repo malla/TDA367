fbb538d376f5652fa6653496debbf8a4022a4710
Johan Gustavsson <johang87@hotmail.com>
2012-03-16 08:42:59 +0100

    Test skapat

--- TDA367/src/projekt/Mission.java
+++ TDA367/src/projekt/Mission.java
@@ -2,10 +2,16 @@ package projekt;
 
 public class Mission {
 	
+//	private Team missionTeam;
+	private Card card;
+	private Bet bet;
+	
 	public Mission(){
-		
+		bet = new Bet(4);
+		card = new Card();
 	}
 	
-	
-
+	public void startMission(){
+		
+	}
 }
