ff7e1edee89ba7f5b100a1d6390989cab8495cbb
Madeleine Appert <m.appert@hotmail.com>
2012-03-19 18:58:28 +0100

    Fixed Timer, made graphical representation

---
+++ TDA367/src/projekt/Kladdklass.java
@@ -0,0 +1,25 @@
+package projekt;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Kladdklass {
+	
+	
+	
+	
+	
+//	countDown();
+	
+	private void countDown(){
+		for (int i=30; i>0; --i){
+		System.out.println(i);
+			//	timerPanel.repaint();
+		}
+	}
+
+}
