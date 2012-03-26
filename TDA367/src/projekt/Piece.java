59f3c21c21f97c44fb2f3e8584d2aa5d1ed07146
Cecilia Edwall <edwalls@live.se>
2012-03-26 15:25:10 +0200

    my files

--- TDA367/src/projekt/Piece.java
+++ TDA367/src/projekt/Piece.java
@@ -1,8 +1,26 @@
 package projekt;
 
+import java.awt.Color;
+
 public class Piece {
-private int position;
-public void movePiece(int i){
-position=position+i;
-}
+	private int position;
+	private int piece;
+	
+	public void movePieceForward(int steps){
+		position = position + steps;
+	}
+	public void movePieceBackward(){
+		position = position - 2;
+	}
+	
+	public void setColor(Color col){
+		
+	}
+	public void setPiece(int piece) {
+		this.piece = piece;
+	}
+	
+	public int getPiece() {
+		return piece;
+	}
 }
--- TDA367/src/projekt/Team.java
+++ TDA367/src/projekt/Team.java
@@ -1,11 +1,18 @@
 package projekt;
 
-public class Team {
+public class Team extends Piece {
 	private int piece;
-	private int board;
-	
-	public void getPiece(String pieceColor){
-		
+	private Object board;
+	public void setPiecePos(int piece) {
+		this.piece = piece;
 	}
-	
+	public int getPiecePos() {
+		return piece;
+	}
+	public void setBoard(String board) {
+		this.board = board;
+	}
+	public Object getBoard() {
+		return board;
+	}
 }
