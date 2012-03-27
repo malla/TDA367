1766287fb14195804bc557c36fc3a583a6987df6
Cecilia Edwall <edwalls@live.se>
2012-03-27 13:58:07 +0200

    my files

--- TDA367/src/projekt/Piece.java
+++ TDA367/src/projekt/Piece.java
@@ -5,12 +5,20 @@ import java.awt.Color;
 public class Piece {
 	private int position;
 	private int piece;
+	private Color color1;
 	
 	public void movePieceForward(int steps){
 		position = position + steps;
 	}
 	public void movePieceBackward(){
 		position = position - 2;
+	}
+	public Color color(Color i){
+		Color color = new Color(piece);
+		for(int n = 0; n <= 8; n++){
+			Color color1 = new Color(n);
+		}
+		return color;
 	}
 	
 	public void setColor(Color col){
