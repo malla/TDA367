4ebccd56158396100d24bc0878af8876d9f53294
Cecilia Edwall <edwalls@live.se>
2012-03-28 21:08:49 +0200

    Changes to Sasses classes

--- TDA367/src/projekt/Piece.java
+++ TDA367/src/projekt/Piece.java
@@ -1,44 +1,63 @@
 package projekt;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Piece {
-<<<<<<< HEAD
+public class Piece implements ActionListener {
 	private int position;
 	private int piece;
-	private Color color1;
+	private int n;
+	private Color color;
 	
+	/*public Piece(){
+		piece.addActionListener(this);
+	}*/
+	
+	/**
+	 * Moves the piece forward depending on how many steps the player bet. 
+	 * @param steps
+	 */
 	public void movePieceForward(int steps){
 		position = position + steps;
 	}
+	/**
+	 * Move the piece backward if the player doesn't accomplish the bet.
+	 */
 	public void movePieceBackward(){
 		position = position - 2;
 	}
-	public Color color(Color i){
-		Color color = new Color(piece);
-		for(int n = 0; n <= 8; n++){
-			Color color1 = new Color(n);
-		}
-		return color;
+	/**
+	 * List the colors and removes the color that already been taken. 
+	 * @param choCol
+	 */
+	//I want to list the different color and if one is taken it's place should be replaced with null. 
+	public void chooseColor(){
+		Color[] color = {Color.green, Color.blue, Color.magenta, Color.yellow, Color.red, Color.orange, Color.black, Color.white};
 	}
-	
-	public void setColor(Color col){
-		
+	/**
+	 * Set the chosen color to the players piece. 
+	 */
+	public void setColor(Color color){
+		this.color = color;
 	}
+	/**
+	 * Sets the piece on given place on the board.
+	 * @param piece
+	 */
 	public void setPiece(int piece) {
 		this.piece = piece;
 	}
-	
+	/**
+	 * Gets the place of the piece on the board. 
+	 * @return
+	 */
 	public int getPiece() {
 		return piece;
 	}
-=======
-/**
- * @uml.property  name="position"
- */
-private int position;
-public void movePiece(int i){
-position=position+i;
-}
->>>>>>> 5c90a0746afa7916df4cda89a07bae563be2ceb1
+	@Override
+	public void actionPerformed(ActionEvent evt) {
+		// TODO Auto-generated method stub
+		
+	}
 }
