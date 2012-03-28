6f829b8cb2203345af8ce56f541d69b843c76a58
Bella <isabellefrolich@hotmail.com>
2012-03-28 22:23:42 +0200

    Changes to Controler and Model

--- TDA367/src/projekt/Board.java
+++ TDA367/src/projekt/Board.java
@@ -5,18 +5,26 @@ import java.awt.Color;
 import java.util.Random;
 
 public class Board{
+	private Piece pieceOne;
+	private Piece pieceTwo;
 	
 	/**
 	 * @uml.property  name="tileTypes" multiplicity="(0 -1)" dimension="1"
 	 */
 	Color[] tileTypes ={Color.BLUE, Color.GREEN, Color.YELLOW, Color.RED};
+	
 	/**
 	 * @uml.property  name="boardArray"
 	 * @uml.associationEnd  multiplicity="(0 -1)"
 	 */
 	Tile [] boardArray = new Tile[48];	
+	Piece[][] piecePositions = new Piece[2][boardArray.length];
 	
 	public Board(){
+		this.pieceOne = new Piece();
+		this.piecePositions[0][0] = pieceOne;
+		this.pieceTwo = new Piece();
+		this.piecePositions[1][0] = pieceTwo;
 		Random randomTiles = new Random();
 		
 		for(int i=0; i<48; i++){
