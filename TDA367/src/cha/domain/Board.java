f04ce98576f34f5eb0009300b63c5229b9858009
Bella <isabellefrolich@hotmail.com>
2012-05-18 12:27:16 +0200

    Test classes added

--- TDA367/src/cha/domain/Board.java
+++ TDA367/src/cha/domain/Board.java
@@ -31,11 +31,8 @@ public class Board{
 	/**
 	 * @uml.property  name="tileTypes" multiplicity="(0 -1)" dimension="1"
 	 */
-	// What is this?
+
 	
-	//TODO Check if this is a proper solution, 
-	// (use integer-constants with the enum names, and use Random to randomize a Category?)
-	// �r det inte det vi g�r?
 	private ArrayList<Category> categoryList = new ArrayList<Category>();
 	private final Color[] colorList = new Color[]{
 			Color.WHITE,
@@ -54,6 +51,8 @@ public class Board{
 	
 	private static Board instance = null;
 
+	//Singleton-pattern
+	
 	public static Board getInstance() {
 		if (instance == null) {
 			instance = new Board();
@@ -69,7 +68,7 @@ public class Board{
 	// Constructor
 	
 	private Board(){
-		//TODO Check if proper method and if we should place it somewhere else
+		
 		this.categoryList.add(Category.SAMECLASS);
 		this.categoryList.add(Category.BODYTOBODY);
 		this.categoryList.add(Category.WORDJUMBLE);
