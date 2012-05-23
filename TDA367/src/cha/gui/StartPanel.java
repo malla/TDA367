f2cd359e85e3ee82665268f4212769effb90a97d
Cecilia Edwall <edwalls@live.se>
2012-05-23 12:40:48 +0200

    update

--- TDA367/src/cha/gui/StartPanel.java
+++ TDA367/src/cha/gui/StartPanel.java
@@ -8,6 +8,7 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
