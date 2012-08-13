22e7510af76ee9e0b4c86643858f1f6e45fa0979
Madeleine Appert <m.appert@hotmail.com>
2012-08-13 21:39:01 +0200

    lalal

--- TDA367/src/cha/gui/ChallengePanel.java
+++ TDA367/src/cha/gui/ChallengePanel.java
@@ -7,12 +7,12 @@ import cha.domain.Challenge;
 import cha.domain.Piece;
 
 public class ChallengePanel {
-	
+
 	public ChallengePanel(){
 		String inputOppTeam;
 		int numberOfTeams = Board.getInstance().getNumberOfPieces();
 		Piece oppTeam;
-		
+
 		while (true) {
 			try {
 				inputOppTeam = JOptionPane.showInputDialog("Which team would you like to compete against?");
@@ -20,22 +20,24 @@ public class ChallengePanel {
 				if (inputOppTeam == null){
 					return;
 				}
-				
+
 				for(int i = 0; i < numberOfTeams; i++){
 					if(inputOppTeam.contains(Board.getInstance().getTeamName(i))){
 						oppTeam = Board.getInstance().getPiece(i);
-					break;
+						break;
 					}
 				}
-				}				
-				catch (NumberFormatException e) {
+			}				
+			catch (NumberFormatException e) {
 			}
 
 			JOptionPane.showMessageDialog(null, "There is no such team");
 		}
-		
-		
+
+
 		Board.StartChallenge(oppTeam);
-		
+
+
+
 	}
 }
