a32f64cb77b6851f495df700069b6002706413d3
Madeleine Appert <m.appert@hotmail.com>
2012-08-15 21:43:08 +0200

    Just taking away the code accidently added by merge

--- TDA367/src/cha/domain/Challenge.java
+++ TDA367/src/cha/domain/Challenge.java
@@ -9,14 +9,10 @@ import cha.event.IEventHandler;
 public class Challenge implements IEventHandler {
 	private final Piece challenger;
 	public Piece opponent;
-	Category category;
-
-
-
+	public Category category;
 	public static Mission chaMission;
 	public static Mission oppMission;
 	private Bet maxBet = new Bet(7);
-
 	public int chaScore;
 	public int oppScore;
 	public String resultString;
@@ -25,30 +21,14 @@ public class Challenge implements IEventHandler {
 
 	//Challenge fönster kommer upp, frågar vem som utmanas, följande kallas därefter.
 	public Challenge(Piece activePiece, Piece opponent, Category c){
-
-	
 		EventBus.getInstance().register(this);
 		chaScore = 11;
 		oppScore = 11;
-
 		this.challenger = activePiece;
 		this.opponent = opponent;
 		this.category = c;
 		startChallenge(); // Duellen startar...
 	}
-
-
-//	private void startChallenge(){
-//		Board.setChallenge(true);
-//		chaMission = new Mission(challenger, category, maxBet);
-//		oppMission = new Mission(opponent, category, maxBet);
-//		chaMission.startMission();
-//		setChaScore(ChallengePanel.pointsEarned());
-//		oppMission.startMission();
-//		setOppScore(ChallengePanel.pointsEarned());
-//		getResult();
-//		Board.setChallenge(false);
-//		//oppMission.setChallenge(false);
 
 	/**
 	 * This method starts the Challenge, only by creating the first mission and
