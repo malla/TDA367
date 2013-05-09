a8b69b49e15b8eb82fa4d041e9953e29c0462bdb
Madeleine Appert <m.appert@hotmail.com>
2013-05-09 22:34:28 +0200

    Check main and do what is left

--- TDA367/src/cha/Main.java
+++ TDA367/src/cha/Main.java
@@ -13,10 +13,8 @@ public class Main {
 
 /** KVAR ATT GÖRA:
  *  - Ta bort onödiga System.out.print rader som använts för kontroll. 
- *  - Se till att texten ändras efter att turen gått över efter challenge.
- *  - Färger - Två lag ska inte kunna vara samma färg.
- *  - Piece hoppar av sig själv när någon står på en Challenge-tile
- *  - Den repaintar inte efter en Challenge innan det blir ny Challenge tur.
+ *  - Fixa back knappen i regler menyn. 
+ *  TESTER
  *
  *	Johan:
  *	Mission/challenge UC	//DONE
