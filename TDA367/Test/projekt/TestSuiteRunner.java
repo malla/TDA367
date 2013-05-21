1d9cb15014e305d8f0b1467cc9273c602443b95f
Madeleine Appert <m.appert@hotmail.com>
2013-05-21 19:16:40 +0200

    NormalTurnTest Done

--- TDA367/Test/projekt/TestSuiteRunner.java
+++ TDA367/Test/projekt/TestSuiteRunner.java
@@ -5,11 +5,12 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import cha.domain.CardTest;
+import cha.domain.DequeTest;
 
 	
 	@RunWith(Suite.class)
 	@SuiteClasses({ TestBoard.class, CardTest.class, 
 		cha.domain.ChallengeTest.class,cha.domain.PieceTest.class,cha.domain.TurnTest.class,
 		cha.domain.MissionTest.class,cha.domain.BetTest.class,
-		TestDeque.class,  TestTeam.class, TestTile.class})
+		DequeTest.class,  TestTeam.class, TestTile.class})
 	public class TestSuiteRunner {}
